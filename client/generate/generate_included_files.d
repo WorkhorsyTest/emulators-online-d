2de1f3824b3bfb5fd9b97adefd94d0526f7f1061
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-03-25 12:53:40 -0700

    Extracted the data encoding into its own functions.

--- client/generate/generate_included_files.d
+++ client/generate/generate_included_files.d
@@ -16,44 +16,89 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import std.conv;
 import std.stdio;
 import std.file;
-import std.string;
-import std.base64;
-import std.zlib;
-import std.process;
-import std.array : appender;
-import cbor;
 
 
-void CompressWith7zip(string in_file, string out_file) {
-	import std.algorithm;
-	import std.array;
+enum CompressionType {
+	Zlib,
+	Lzma,
+}
 
-	// Get the command and arguments
-	const string[] command = [
-		"../../7za.exe",
-		"a",
-		"-t7z",
-		"-m0=lzma2",
-		"-mx=9",
-		"%s".format(out_file),
-		"%s".format(in_file),
-	];
+ubyte[] ToCompressed(ubyte[] blob, CompressionType compression_type) {
+	final switch (compression_type) {
+		case CompressionType.Lzma:
+			import std.algorithm;
+			import std.array;
+			import std.process;
+			import std.string;
 
-	// Run the command and wait for it to complete
-	auto pipes = pipeProcess(command, Redirect.stdout | Redirect.stderr);
-	int status = wait(pipes.pid);
+			string blob_file = std.file.tempDir() ~ "blob";
+			string zip_file = std.file.tempDir() ~ "blob.7z";
 /*
-	string[] result_stdout = pipes.stdout.byLine.map!(l => l.idup).array();
-	string[] result_stderr = pipes.stderr.byLine.map!(l => l.idup).array();
-	stdout.writefln("!!! stdout:%s", result_stdout);
-	stdout.writefln("!!! stderr:%s", result_stderr);
+			stdout.writefln("blob_file; %s", blob_file);
+			stdout.writefln("zip_file; %s", zip_file);
 */
-	if (status != 0) {
-		stderr.writefln("Failed to run command: %s\r\n", "7za.exe");
+
+			// Write the blob to file
+			std.file.write(blob_file, blob);
+
+			// Get the command and arguments
+			const string[] command = [
+				"7za.exe",
+				"a",
+				"-t7z",
+				"-m0=lzma2",
+				"-mx=9",
+				"%s".format(zip_file),
+				"%s".format(blob_file),
+			];
+
+			// Run the command and wait for it to complete
+			auto pipes = pipeProcess(command, Redirect.stdout | Redirect.stderr);
+			int status = wait(pipes.pid);
+		/*
+			string[] result_stdout = pipes.stdout.byLine.map!(l => l.idup).array();
+			string[] result_stderr = pipes.stderr.byLine.map!(l => l.idup).array();
+			stdout.writefln("!!! stdout:%s", result_stdout);
+			stdout.writefln("!!! stderr:%s", result_stderr);
+		*/
+			if (status != 0) {
+				stderr.writefln("Failed to run command: %s\r\n", "7za.exe");
+			}
+
+			// Read the compressed blob from file
+			ubyte[] file_data = cast(ubyte[]) std.file.read(zip_file);
+
+			// Delete the temp files
+			std.file.remove(blob_file);
+			std.file.remove(zip_file);
+
+			return file_data;
+	case CompressionType.Zlib:
+		import std.zlib;
+		ubyte[] zlibed_data = std.zlib.compress(blob, 9);
+		return zlibed_data;
 	}
+}
+
+ubyte[] ToCompressedBase64(T)(T thing, CompressionType compression_type) {
+	import std.array : appender;
+	import cbor;
+	import std.base64;
+
+	// Convert the thing to a blob
+	auto buffer = appender!(ubyte[])();
+	encodeCbor(buffer, thing);
+	ubyte[] blob = buffer.data;
+
+	// Compress the blob
+	ubyte[] compressed_bob = ToCompressed(blob, compression_type);
+
+	// Base64 the compressed blob
+	ubyte[] base64ed_compressed_blob = cast(ubyte[]) Base64.encode(compressed_bob);
+
+	return base64ed_compressed_blob;
 }
 
 int main() {
@@ -104,23 +149,7 @@ int main() {
 	}
 
 	// Convert the array to a blob
-	auto buffer = appender!(ubyte[])();
-	encodeCbor(buffer, file_map);
-
-	// Write the blob to file
-	std.file.write("client/generated/blob", buffer.data);
-	//buffer = null;
-
-	// Compress the blob to file
-	std.file.chdir("client/generated");
-	CompressWith7zip("blob", "blob.7z");
-	std.file.chdir("../..");
-
-	// Read the compressed blob from file
-	ubyte[] file_data = cast(ubyte[]) std.file.read("client/generated/blob.7z");
-
-	// Base64 the compressed blob
-	ubyte[] base64ed_data = cast(ubyte[]) Base64.encode(file_data);
+	ubyte[] base64ed_data = ToCompressedBase64(file_map, CompressionType.Lzma);
 
 	// Write the files generating function
 	output.write("string GetCompressedFiles() {\r\n");
@@ -130,17 +159,10 @@ int main() {
 	output.write("}\r\n");
 
 	// Read 7zip into an array
-	file_data = cast(ubyte[]) std.file.read("7za.exe");
+	ubyte[] file_data = cast(ubyte[]) std.file.read("7za.exe");
 
 	// Convert the 7zip array to a blob
-	buffer = appender!(ubyte[])();
-	encodeCbor(buffer, file_data);
-
-	// Compress the blob
-	ubyte[] zlibed_data = std.zlib.compress(buffer.data, 9);
-
-	// Base64 the compressed blob
-	base64ed_data = cast(ubyte[]) Base64.encode(zlibed_data);
+	base64ed_data = ToCompressedBase64(file_data, CompressionType.Zlib);
 
 	// Write the 7zip generating function
 	output.write("string GetCompressed7zip() {\r\n");
