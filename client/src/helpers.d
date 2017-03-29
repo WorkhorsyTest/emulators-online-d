33746b00590224873150179fe74328fae56f8375
Matthew Brennan Jones <matthew.brennan.jones@gmail.com>
2017-03-29 14:26:05 -0700

    Made it only check for the DirectX version on Windows.

--- client/src/helpers.d
+++ client/src/helpers.d
@@ -44,40 +44,44 @@ private void backgroundThread(Tid ownerTid) {
 }
 
 int GetDirectxVersion() {
-	import std.process;
-	import std.file;
-	import std.stdio;
-	import std.string;
-
-	const string[] command = [
-		"dxdiag.exe",
-		"/t",
-		"directx_info.txt",
-	];
-
-	// Run the command and wait for it to complete
-	auto pipes = pipeProcess(command, Redirect.stdout | Redirect.stderr);
-	int status = wait(pipes.pid);
-
-	if (status != 0) {
-		stderr.writefln("Failed to determine DirectX version"); stderr.flush();
-	}
-
-	string string_data = cast(string) std.file.read("directx_info.txt");
-	string raw_version = string_data.split("DirectX Version: ")[1].split("\r\n")[0];
-
-	// Get the DirectX version
 	int int_version = -1;
-	if (raw_version.indexOf("12") != -1) {
-		int_version = 12;
-	} else if (raw_version.indexOf("11") != -1) {
-		int_version = 11;
-	} else if (raw_version.indexOf("10") != -1) {
-		int_version = 10;
-	} else if (raw_version.indexOf("9") != -1) {
-		int_version = 9;
-	} else {
-		stderr.writefln("Failed to determine DirectX version"); stderr.flush();
+
+	version (Windows) {
+		import std.process;
+		import std.file;
+		import std.stdio;
+		import std.string;
+
+		const string[] command = [
+			"dxdiag.exe",
+			"/t",
+			"directx_info.txt",
+		];
+
+		// Run the command and wait for it to complete
+		auto pipes = pipeProcess(command, Redirect.stdout | Redirect.stderr);
+		int status = wait(pipes.pid);
+
+		if (status != 0) {
+			stderr.writefln("Failed to determine DirectX version"); stderr.flush();
+		}
+
+		string string_data = cast(string) std.file.read("directx_info.txt");
+		string raw_version = string_data.split("DirectX Version: ")[1].split("\r\n")[0];
+
+		// Get the DirectX version
+		if (raw_version.indexOf("12") != -1) {
+			int_version = 12;
+		} else if (raw_version.indexOf("11") != -1) {
+			int_version = 11;
+		} else if (raw_version.indexOf("10") != -1) {
+			int_version = 10;
+		} else if (raw_version.indexOf("9") != -1) {
+			int_version = 9;
+		} else {
+			stderr.writefln("Failed to determine DirectX version"); stderr.flush();
+		}
 	}
+
 	return int_version;
 }
